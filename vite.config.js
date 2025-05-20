cc4a36bb04cd5a0ce4558fbd993140fd974c01e8
NUSER434 <120679697+NUSER434@users.noreply.github.com>
2025-05-21 02:57:42 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,22 +5,8 @@ export default defineConfig({
     plugins: [
         laravel({
             input: [
-                // Основные файлы
                 'resources/css/app.css',
                 'resources/js/app.js',
-
-                // Дополнительные стили
-                'resources/css/header1.css',
-                'resources/css/home.css',
-                'resources/css/profile.css',
-
-                // Дополнительные скрипты
-                'resources/js/about.js',
-                'resources/js/caruselt.js',
-                'resources/js/headert.js',
-                'resources/js/populart.js',
-                'resources/js/profilet.js',
-                'resources/js/slidert.js',
             ],
             refresh: true,
         }),
