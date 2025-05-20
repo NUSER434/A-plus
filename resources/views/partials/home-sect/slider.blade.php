422577221aafe599c4a91b66bcfc799f394a8a68
NUSER434 <120679697+NUSER434@users.noreply.github.com>
2025-05-21 03:53:05 +0500

    Update slider.blade.php

--- resources/views/partials/home-sect/slider.blade.php
+++ resources/views/partials/home-sect/slider.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    @vite(['resources/js/slider.js'])
+    <script src="/build/assets/app-CiU_oirN.js" defer></script>
     
 </head>
 <body class="bg-gray-900">
