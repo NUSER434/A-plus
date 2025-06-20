0508f4170be1bb561b5158ec753668b7d1c18d69
NUSER434 <120679697+NUSER434@users.noreply.github.com>
2025-06-20 19:17:07 +0500

    Update CartController.php

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\Order;
 use App\Models\CompletedOrder;
+use App\Models\OrderDetail;
 
 class CartController extends Controller
 {
